5291990cab6538a675571f52e8e59d8439e6d585
Alex <44046325+suzhen99@users.noreply.github.com>
2019-04-11 10:30:47 +0800

    Update icrm_Win.bat

--- icrm_Win.bat
+++ icrm_Win.bat
@@ -1,8 +1,9 @@
 @ECHO OFF
 
+ECHO Please install Python2.7 and git
 SET Course_Name=CL210
-SET Course_Dir="D:\INSTRUCTOR"
-SET GIT_CMD="C:\Program Files\Git\bin\git.exe"
+SET Course_Dir=D:\INSTRUCTOR
+SET PATH=%PATH%;"%ProgramFiles%\Git\bin;"%ProgramFiles%\7-Zip"
 
 
 ECHO Install follow three using PIP
@@ -12,7 +13,6 @@ pip install requests -q
 pip install lxml -q
 
 ECHO git icrm
-SET PATH=%PATH%;"%ProgramFiles%\Git\bin"
 git init RHCI && cd RHCI
 git config core.sparsecheckout true
 echo 'icrm*' >> .git/info/sparse-checkout
@@ -20,14 +20,15 @@ git remote add origin https://github.com/suzhen99/RHCI.git
 git pull origin master
 
 ECHO extract icrm
-SET PATH=%PATH%;"C:\Program Files\7-Zip"
 7z e icrm-1.0.7-2.el7.noarch.rpm
 7z x icrm-1.0.7-2.el7.noarch.cpio -y
 
 ECHO Prepare bin and config
 python usr\lib\python2.7\site-packages\icrm\__init__.py help > NULL
-echo --- > ..\.icrm\config.yml
-echo repository: %Course_Dir%\%Course_Name% >> ../.icrm/config.yml
+
+for /F %%i in ('dir /d /b %Course_Dir%\%Course_Name%*') do (set CNF=%%i)
+echo --- > %USERPROFILE%\.icrm\config.yml
+echo repository: %Course_Dir%\%CNF% >> %USERPROFILE%\.icrm\config.yml
 
 ECHO Run icrm
 python usr\lib\python2.7\site-packages\icrm\__init__.py search %Course_Name%
