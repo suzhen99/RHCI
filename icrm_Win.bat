187b88a64ef58385dc942ecace194f9554445508
Alex <44046325+suzhen99@users.noreply.github.com>
2019-04-11 10:17:03 +0800

    Update icrm_Win.bat

--- icrm_Win.bat
+++ icrm_Win.bat
@@ -5,32 +5,29 @@ SET Course_Dir="D:\INSTRUCTOR"
 SET GIT_CMD="C:\Program Files\Git\bin\git.exe"
 
 
-# Install follow three using PIP
+ECHO Install follow three using PIP
 pip install pyyaml==3.11 -q
 pip install pyreadline==2.0 -q
 pip install requests -q
 pip install lxml -q
 
-# Extract icrm
-%GIT_CMD% init RHCI && cd RHCI
-%GIT_CMD% config core.sparsecheckout true
-echo 'tt*' >> .git/info/sparse-checkout
-%GIT_CMD% remote add origin git@github.com:mygithub/test.git
-%GIT_CMD% pull origin master
+ECHO git icrm
+SET PATH=%PATH%;"%ProgramFiles%\Git\bin"
+git init RHCI && cd RHCI
+git config core.sparsecheckout true
+echo 'icrm*' >> .git/info/sparse-checkout
+git remote add origin https://github.com/suzhen99/RHCI.git
+git pull origin master
 
-wget https://github.com/suzhen99/RHCI/raw/master/icrm-1.0.7-2.el7.noarch.rpm
-rpm2cpio icrm-1.0.7-2.el7.noarch.rpm | cpio -dium
+ECHO extract icrm
+SET PATH=%PATH%;"C:\Program Files\7-Zip"
+7z e icrm-1.0.7-2.el7.noarch.rpm
+7z x icrm-1.0.7-2.el7.noarch.cpio -y
 
-# Prepare bin
-sudo ln -s /usr/bin/python2.7 /usr/bin/python2
-sudo cp -r usr/lib/python2.7/site-packages/icrm /usr/lib/python2.7
-sudo sed -ie '/setterm/s|".*"|"echo"|' /usr/lib/python2.7/icrm/__init__.py
-sudo cp usr/bin/icrm /usr/local/bin
+ECHO Prepare bin and config
+python usr\lib\python2.7\site-packages\icrm\__init__.py help > NULL
+echo --- > ..\.icrm\config.yml
+echo repository: %Course_Dir%\%Course_Name% >> ../.icrm/config.yml
 
-# Prepare config
-icrm help >/dev/null
-Course_DN=$(ls -d "${Course_Dir}"/${Course_Name}*)
-sed -ie "/repository/s|:.*|: ${Course_DN}|" ~/.icrm/config.yml
-
-# Run icrm
-icrm search ${Course_Name}
+ECHO Run icrm
+python usr\lib\python2.7\site-packages\icrm\__init__.py search %Course_Name%
